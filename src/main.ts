0b5f7f4c29547b6522003f7d7880ecac8fe23962
Munsy <tmunson@wsu.edu>
2025-11-29 01:30:47 -0800

    ng 18

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 
