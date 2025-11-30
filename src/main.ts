4aab0f3f88544ac176b75165f6be8f9be8577b13
Munsy <tmunson@wsu.edu>
2025-11-29 22:18:38 -0800

    erm

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 // src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(appRoutes),
+    provideZoneChangeDetection(),provideRouter(appRoutes),
     importProvidersFrom(HttpClientModule),
   ],
 });
